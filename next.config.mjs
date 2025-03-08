95c0bedd1b3649f8db935e851a7a2f52d544fdaf
Sina Nakhaei <cna.nakhaei@gmail.com>
2025-03-08 18:29:55 +0330

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,6 @@ export default {
   output: "export", // Enables static site generation
   images: { unoptimized: true }, // Required for GitHub Pages
   basePath: "/thesinaa", // Change this to your GitHub repo name
+  assetPrefix: "/thesinaa/",
   trailingSlash: true, // Helps avoid routing issues
 };
