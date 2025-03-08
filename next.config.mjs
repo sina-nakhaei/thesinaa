5adec1b993e28d9f7ada9d1f2b41ed34165acfbc
Sina Nakhaei <cna.nakhaei@gmail.com>
2025-03-08 13:56:13 +0330

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+export default {
+  output: "export", // Enables static site generation
+  images: { unoptimized: true }, // Required for GitHub Pages
+  basePath: "/thesinaa", // Change this to your GitHub repo name
+  trailingSlash: true, // Helps avoid routing issues
 };
-
-export default nextConfig;
