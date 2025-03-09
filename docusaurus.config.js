5b49af996568f0a0a554e8b0e197c94a2a529655
Sina Nakhaei <cna.nakhaei@gmail.com>
2025-03-10 00:24:43 +0330

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sina-nakhaei', // Usually your GitHub org/user name.
   projectName: 'thesinaa', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
