af0ad41bf68be463d85bca74e08ca059d799539e
Sina <cna.nakhaei@gmail.com>
2025-03-09 10:32:30 +0330

    change basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 export default {
   output: "export", // Enables static site generation
   images: { unoptimized: true }, // Required for GitHub Pages
-  basePath: "/thesinaa", // Change this to your GitHub repo name
-  assetPrefix: "/thesinaa/",
+  basePath: "/", // Change this to your GitHub repo name
+  assetPrefix: "/",
   trailingSlash: true, // Helps avoid routing issues
 };
