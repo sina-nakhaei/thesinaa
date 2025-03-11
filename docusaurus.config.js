c2086f12c99705843c34472192a2dd5dea177484
Sina Nakhaei <cna.nakhaei@gmail.com>
2025-03-11 21:11:20 +0330

    Update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://sina-nakhaei.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/thesinaa/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
